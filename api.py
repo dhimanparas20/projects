8d840f064d1fcfd5b5f542c829417c73432ff0f5
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2022-08-13 20:13:32 +0530

    Python API with requests
    Basic syntax for  working with Python API  using requests module .
    *This code returns the Name of the github user.

---
+++ api.py
@@ -0,0 +1,17 @@
+# Inport Libraries
+import requests, json 
+from os import system 
+
+# Making a GET request 
+system("clear") #Clears the previous session screen
+uname  =  input("Enter the username: ")
+print("")
+r = requests.get(f"https://api.github.com/users/{uname}")
+x = r.json()  # converts the API call to JSON format
+
+print(f"url link = {r.url}")  #prints url
+print(f"Rsesponse code = {r.status_code}") #prints status code
+print(f"Response code less than 200 = {r.ok}")  #Returns True if status_code is less than 200
+print(f"user name = {x['name']}")  #Prints username
+print("")
+print(x) # prints the whole JSON structure
