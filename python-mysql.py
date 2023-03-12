a2b257542b0743254d2d350cc3db74b46c453654
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2023-03-12 18:58:23 +0530

    Code betterment

--- python-mysql.py
+++ python-mysql.py
@@ -4,28 +4,36 @@
 import mysql.connector as mc
 from mysql.connector import Error
 from os import system
+
 system("clear")
 
 # MYSQL VARIABLES
 HOST="localhost"
 USER="root"
-PASS=""
-DATABASE=""
-TABLE=""
+PASS="Luffykiid@2069"
+DATABASE="rockonrescue"
+TABLE="rescue"
 
 try:
   db = mc.connect(host='localhost',
                   database=DATABASE,
                   user=USER,
                   password=PASS)
-  mc=db.cursor()   
+  #mc=db.cursor()
+  mc=db.cursor(dictionary=True)
 
-  if db.is_connected():  #Show DB status
-    db_Info = db.get_server_info()
-    print("Connected to MySQL Server version ", db_Info)
-    mc.execute("select database();")
-    record = mc.fetchone()
-    print("You're connected to database: ", record)
+
+  def status():
+    if db.is_connected():  #Show DB status
+      db_Info = db.get_server_info()
+      print("------------------------------------------------------------------")
+      print("Connected to MySQL Server version ", db_Info)
+      mc.execute("select database();")
+      record = mc.fetchone()
+      print("You're connected to database: ", record)
+      print("------------------------------------------------------------------")
+    else:
+      print("error")  
 
   def create_table():   #Create table
     sql = f"create table {TABLE} (   \
@@ -34,46 +42,58 @@ try:
             age int, \
             descr varchar(50), \
             adopted varchar(3), \
-            ext varchar(5))"    
+            ext varchar(5))"
     mc.execute(sql)
-    db.commit()          
-  
+    db.commit()
+
   def table_insert(id,name,age,descr,adopted,ext):   #insert records
     sql = f"INSERT INTO {TABLE} (id,name,age,descr,adopted,ext) VALUES (%s, %s, %s, %s, %s, %s)"
     val = (id,name,age,descr,adopted,ext)
     mc.execute(sql, val)
     db.commit()
+    return({"status":"success"})
 
   def table_update(id,parameter,value):      #Update Records
     sql = f"UPDATE {TABLE} SET {parameter} = %s WHERE id = %s"
     val = (value,id)
     mc.execute(sql,val)
-    db.commit() 
+    db.commit()
+    return({"status":"success"})
+
+  def table_updateall(id,name,age,descr,adopted,ext):      #Update Records
+    sql = f"UPDATE {TABLE} SET name=%s,age=%s,descr=%s,adopted=%s,ext=%s WHERE id = %s"
+    val = (name,age,descr,adopted,ext,id)
+    mc.execute(sql,val)
+    db.commit()
+    return({"status":"success"})  
 
   def table_display():           #Display all records
     sql=f"select * from {TABLE} "
     mc.execute(sql)
-    for i in mc:
-      print(i)
-   
+    ab = mc.fetchall()
+    return ab
+
   def table_fetch(id): #fetch a records
     sql=f"select * from {TABLE} where id=%s"
     val = [id]
     mc.execute(sql,val)
-    for i in mc:
-      print(i)
+    for i in mc :
+      #dict = {"_id":i[0],"id":i[1],"name":i[2],"age":i[3],"desc":i[4],"adopted":i[5],"ext":i[6]}
+      #return (dict)
+      return (i)
 
   def table_count():  #returns total number of records
     sql=f"SELECT COUNT(*) FROM {TABLE}"
     mc.execute(sql)
-    for i in mc:
-      print(i)
+    for i in mc :
+      return i["COUNT(*)"]
 
   def table_delete(id):
     sql = f"delete from {TABLE} where  id=%s"
     val = ([id])
     mc.execute(sql,val)
     db.commit()
+    return({"status":"success"})
 
 except Error as e:
   print("Error while connecting to MySQL", e)    
@@ -81,14 +101,9 @@ except Error as e:
 
 #table_insert("hgjg1", "Ramu", 1, "lala lala", "yes", "jpg")
 #table_update("dfsdf","age", 8)
-#table_display()
-#table_fetch("dfsdf")
-#table_count()
+#for i in table_disp():
+#  print(i)
+#print(table_fetch("hgjg1"))
+#print(table_count())
 #table_delete("dfsdf")
-
-    
-
-
-
-
-
+ 
