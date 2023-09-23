7f954cca0d35cc89ea0ae86ecf886090507eeef2
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2023-09-23 15:15:33 +0530

    Create dockerInstallRaspberryPI.sh

---
+++ dockerInstallRaspberryPI.sh
@@ -0,0 +1,9 @@
+# To install Docker in Raspberry PI (rasbbian os)
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=armhf signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/raspbian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker
+sudo systemctl start docker
