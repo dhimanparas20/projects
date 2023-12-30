19dd60135cb43c1248101067ff4c64e56afe09ba
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2023-12-30 23:10:33 +0530

    Update installMQTT.sh

--- installMQTT.sh
+++ installMQTT.sh
@@ -11,11 +11,13 @@ sudo systemctl enable mosquitto.service
 systemctl status mosquitto.service
 sudo -i
 echo "persistence true" >> /etc/mosquitto/mosquitto.conf
-echo "allow_anonymous true" >> /etc/mosquitto/mosquitto.conf
+echo "allow_anonymous false" >> /etc/mosquitto/mosquitto.conf
+echo "password_file /etc/mosquitto/passwd" >> /etc/mosquitto/mosquitto.conf
 echo "listener 1883 0.0.0.0" >> /etc/mosquitto/conf.d/protocols.conf
 echo "protocol mqtt" >> /etc/mosquitto/conf.d/protocols.conf
 echo "listener 1884 0.0.0.0" >> /etc/mosquitto/conf.d/protocols.conf
 echo "protocol websockets" >> /etc/mosquitto/conf.d/protocols.conf
 exit
+sudo mosquitto_passwd -c /etc/mosquitto/passwd mst
 sudo ufw allow 1883/tcp
 sudo systemctl restart mosquitto.service
